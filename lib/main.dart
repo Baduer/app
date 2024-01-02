98eec5b432d2f02198aa246a7dfc02a4ff376778
Gabriel Inacio <gabriel.sert9@gmail.com>
2024-01-02 15:30:05 +0100

    sing in update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+
 import 'src/auth/sign_in_screen.dart';
-//import 'src/auth/sign_up_screen.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-             
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: false,
+        useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
       home: const SignInScreen(),
--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,122 +1,133 @@
-//Tela de Login
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/auth/components/app_assets.dart';
 import 'package:flutter_application_1/src/auth/components/custom_text_field.dart';
-import 'package:flutter_application_1/src/auth/components/menu_icons.dart';
 import 'package:flutter_application_1/src/auth/sign_up_screen.dart';
-import '../auth/components/class_strings.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'components/gradient_line.dart';
+import 'components/singup_button.dart';
+import 'components/strings.dart';
 
-class SignInScreen extends StatefulWidget {
+class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
-  State<SignInScreen> createState() => _SignInScreenState();
-}
-
-class _SignInScreenState extends State<SignInScreen> {
-  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(children: [
-          //Icon Calendar
-          Padding(
-            padding: const EdgeInsets.only(top: 60),
-            child: Container(
-              alignment: Alignment.center,
-              child: const Icon(
-                Icons.calendar_month,
-                size: 220,
-                color: Colors.purple,
+        physics: const BouncingScrollPhysics(),
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: height,
+              maxWidth: 720,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      FontAwesomeIcons.arrowRightToBracket,
+                      size: height * 0.20,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  const Spacer(),
+                  const CustomTextField(
+                    icon: Icons.email,
+                    label: Strings.email,
+                  ),
+                  const SizedBox(height: 16),
+                  const CustomTextField(
+                    icon: Icons.lock,
+                    isObscure: true,
+                    label: Strings.lock,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        Strings.newPassword,
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.tertiary),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 48,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        Strings.login,
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.background),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  Row(
+                    children: [
+                      GradientLine.endToStart(context),
+                      const SizedBox(width: 8),
+                      const Text(Strings.orSignUpWith),
+                      const SizedBox(width: 8),
+                      GradientLine.startToEnd(context),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SignUpButton(
+                        asset: AppAssets.google,
+                        onTap: () {},
+                      ),
+                      SignUpButton(
+                        asset: AppAssets.facebook,
+                        onTap: () {},
+                      ),
+                      SignUpButton(
+                        asset: AppAssets.apple,
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(Strings.dontHaveAnAccount),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const SignUpScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          Strings.registerNew,
+                          style: TextStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                ],
               ),
             ),
           ),
-        
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 32,
-              vertical: 40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment
-                  .stretch, //Esticando o botão com a mesma largura com os campos de texto
-              children: [
-                const CustomTextField(
-                  icon: Icons.email,
-                  label: 'E-mail',
-                ),
-                const CustomTextField(
-                  icon: Icons.lock,
-                  isObscure: true,
-                  label: Strings.lock,
-                ),
-        
-                //Esqueceu a senha
-                //Usando o Align p string ficar no canto direito
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      Strings.newPassword,
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                ),
-        
-                //Botão Login
-                SizedBox(
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    )),
-                    onPressed: () {},
-                    child: const Text(
-                      Strings.login,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-        
-                //Entrar com google e Apple
-                const MenuIcons(),
-                
-                //Cadastro
-                SizedBox(
-                  height: 40,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(17),
-                      ),
-                      side: const BorderSide(
-                        width: 2,
-                        color: Colors.purple,
-        
-                      )
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(builder: 
-                        (context) => const SignUpScreen(),
-                        ),
-                        );                      
-                    },
-                    child: const Text(
-                      Strings.registerNew,
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ]),
+        ),
       ),
     );
   }
